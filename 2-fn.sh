ee00792dced3771d710e8ebc2bad5df049beafd6
0xSapo <oxxsapo@gmail.com>
2026-07-16 21:50:46 -0600

    Update 2-fn.sh

--- 2-fn.sh
+++ 2-fn.sh
@@ -4,6 +4,7 @@ cd "$HOME/Downloads/fn"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/NerdFontsSymbolsOnly.zip
 unzip JetBrainsMono.zip
+rm README.md
 unzip NerdFontsSymbolsOnly.zip
 mkdir -p $HOME/.local/share/fonts
 mv *.ttf $HOME/.local/share/fonts
