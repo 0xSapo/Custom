fea6309a20ada0ec75d9516c772cf4d5e7aca317
0xSapo <oxxsapo@gmail.com>
2026-07-17 10:57:53 -0600

    Update 6-dots.sh

--- 6-dots.sh
+++ 6-dots.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # Alacritty
 mkdir -p $HOME/.config/alacritty
-mv ./alacritty.toml TokyoNight.toml $HOME/.confing/alacritty/
+mv ./alacritty.toml ./TokyoNight.toml $HOME/.config/alacritty/
+
+cat << 'EOF' >> ~/.profile
+if [[ -z "$DISPLAY" ]] && [[ $(tty) = /dev/tty1 ]]; then
+    exec startx
+fi
+EOF
+
+echo "exec dwm" >> ~/.xinitrc
+
