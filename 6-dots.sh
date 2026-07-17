acd2bac4a73619ca29298f099cb3e78a80c5a4ee
0xSapo <oxxsapo@gmail.com>
2026-07-16 20:47:13 -0600

    Update 6-dots.sh

--- 6-dots.sh
+++ 6-dots.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Alacritty
 mkdir -p $HOME/.config/alacritty
-mv ./alacritty.toml TokyoNight.toml $HOME/.confing/alacritty
+mv ./alacritty.toml TokyoNight.toml $HOME/.confing/alacritty/
