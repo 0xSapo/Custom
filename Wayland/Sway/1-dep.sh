c5586ca7d9d85c685401bffa4b3e96320b244d43
Sap0x <cesar.garduno@rubidex.ai>
2026-07-20 16:17:46 -0600

    Adding Sway

---
+++ Wayland/Sway/1-dep.sh
@@ -0,0 +1,28 @@
+sudo apt install -y glslang-tools \
+                    libcairo2-dev \
+                    libcap-dev \
+                    libdbus-1-dev \
+                    libdisplay-info-dev \
+                    libevdev-dev \
+                    libgdk-pixbuf2.0-dev \
+                    libinput-dev \
+                    libjson-c-dev \
+                    libliftoff-dev \
+                    libpam0g-dev \
+                    libpango1.0-dev \
+                    libpcre2-dev \
+                    libpixman-1-dev \
+                    libseat-dev \
+                    libsystemd-dev \
+                    libvulkan-dev \
+                    libwayland-dev \
+                    libwayland-egl1 \
+                    libwlroots-dev \
+                    libxcb-ewmh-dev \
+                    libxkbcommon-dev \
+                    meson \
+                    pkgconf \
+                    scdoc \
+                    tree \
+                    wayland-protocols
+sudo apt build-dep sway
