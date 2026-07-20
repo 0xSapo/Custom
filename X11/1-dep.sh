4c0153c1a932ab711680e3ed5bb11b5f648e3797
Sap0x <cesar.garduno@rubidex.ai>
2026-07-20 16:36:28 -0600

    Fixing format

--- X11/1-dep.sh
+++ X11/1-dep.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade
-# General dependencies
-sudo apt-get install -y libdbusmenu-glib-dev libdbusmenu-gtk3-dev libdbusmenu-qt5-dev build-essential curl git libcairo2-dev libfontconfig1-dev libgdk-pixbuf-2.0-dev libgtk-3-dev libpango1.0-dev libssl-dev libx11-dev libxft-dev libxinerama-dev pkg-config suckless-tools xorg xterm rofi alacritty feh unzip fuse3 libfuse3-dev extrepo
+sudo apt update && sudo apt upgrade
+sudo apt install -y libdbusmenu-glib-dev \
+                    libdbusmenu-gtk3-dev \
+                    libdbusmenu-qt5-dev \
+                    build-essential \
+                    curl \
+                    git \
+                    libcairo2-dev \
+                    libfontconfig1-dev \
+                    libgdk-pixbuf-2.0-dev \
+                    libgtk-3-dev \
+                    libpango1.0-dev \
+                    libssl-dev \
+                    libx11-dev \
+                    libxft-dev \
+                    libxinerama-dev \
+                    pkg-config \
+                    suckless-tools \
+                    xorg \
+                    xterm \
+                    rofi \
+                    alacritty \
+                    feh \
+                    unzip \
+                    fuse3 \
+                    libfuse3-dev \
+                    extrepo
