04f2604ce3ee0afc2f3be80b013917026f2a4264
Sap0x <cesar.garduno@rubidex.ai>
2026-07-20 20:27:02 -0600

    Adding Tools

---
+++ Wayland/Sway/0-secuencer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo ./1-dep.sh && ./2-fn.sh && sudo ./3-nvc.sh && sudo ./4-tools.sh && ./5-dots.sh
---
+++ Wayland/Sway/4-tools.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Create Working directory
+mkdir -p $HOME/Downloads/Tools
+cd $HOME/Downloads/Tools
+
+# Install Golang Latest
+sudo rm -rf /usr/local/go
+wget https://go.dev/dl/go1.26.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.26.5.linux-amd64.tar.gz
+echo PATH="$PATH:/usr/local/go/bin" >> $HOME/.bashrc
+source $HOME/.bashrc
+
+# Caido
+wget https://caido.download/releases/v0.57.1/caido-desktop-v0.57.1-linux-x86_64.deb
+chmod 755 caido-desktop-v0.57.1-linux-x86_64.deb
+sudo apt install -y ./caido-desktop-v0.57.1-linux-x86_64.deb
+# Librewolf
+sudo extrepo enable librewolf && sudo extrepo update librewolf
+sudo apt update && sudo apt install librewolf -y
+
+# Obsidian
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.12.7/obsidian_1.12.7_amd64.deb
+chmod 755 obsidian_1.12.7_amd64.deb
+sudo apt install -y ./obsidian_1.12.7_amd64.deb
