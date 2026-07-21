095aa262953617d2a8ffb48c2fdbecac3e6fa63f
Sap0x <cesar.garduno@rubidex.ai>
2026-07-20 21:02:52 -0600

    Fixing Dots

--- Wayland/Sway/5-dots.sh
+++ Wayland/Sway/5-dots.sh
@@ -3,4 +3,4 @@
 echo "[5] Installing Dots"
 echo "    Dots Alacritty"
 mkdir -p $HOME/.config/alacritt
-cp ../../Dotfiles/alacritty/ ../../Dotfiles/TokyoNight.toml $HOME/.config/alacritty/
+cp -r ../../Dotfiles/alacritty/  $HOME/.config/alacritty/
